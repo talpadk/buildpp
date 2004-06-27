995cd83e8ae036131fb424bc97afb39a741ef400
sftalpa <>
2004-06-27 16:39:14 +0000

    Nolonger uses puclic cvs due to 5 hour sync problem

--- makeFileReleasePackages.sh
+++ makeFileReleasePackages.sh
@@ -2,7 +2,7 @@
 
 cd /tmp
 rm -rf buildpp buildpp.tar.gz buildpp.zip
-cvs -z3 -d :pserver:anonymous@cvs.sf.net:/cvsroot/buildpp export -D now buildpp
+cvs -z3 -d :ext:sftalpa@cvs.sf.net:/cvsroot/buildpp export -D now buildpp
 cd buildpp
 make
 cd ..
