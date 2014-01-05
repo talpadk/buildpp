1917b1480389a5f31bd941380f3b30d91e6df2a6
Visti Andresen <talpa@talpa.dk>
2014-01-05 14:12:07 +0100

    Updated the makeFileReleasePackages.sh to use git

--- makeFileReleasePackages.sh
+++ makeFileReleasePackages.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+# Script to generate a new file release
+# Should only be run on the master branch, with all changes pushed
+
+
+VERSION=`perl -e '$d=\`git describe\`;  $major="?"; $minor="?"; $commit="?"; if ($d =~ /^V(\d+).(\d+)-(\d+)-/) { $major=$1;$minor=$2;$commit=$3 } elsif  ($d =~ /^V(\d+).(\d+)/){ $major=$1;$minor=$2;$commit=0  } print "v$major.$minor.$commit";'`
+
+rm -rf /tmp/buildpp /tmp/buildpp.tar /tmp/buildpp_$VERSION.tar.gz /tmp/buildpp_$VERSION.zip
+
+git archive --format tar --prefix=buildpp_$VERSION/ --output /tmp/buildpp.tar HEAD
 
 cd /tmp
-rm -rf buildpp buildpp.tar.gz buildpp.zip
-cvs -z3 -d :ext:sftalpa@cvs.sf.net:/cvsroot/buildpp export -D now buildpp
-cd buildpp
+tar -xf buildpp.tar
+cd buildpp_$VERSION
+
 make
 cd ..
-tar -cvzf buildpp.tar.gz buildpp
-zip -r buildpp buildpp
+tar -cvzf buildpp_$VERSION.tar.gz buildpp_$VERSION
+zip -r buildpp_$VERSION.zip buildpp_$VERSION
